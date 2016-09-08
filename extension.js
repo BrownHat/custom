0aa27f589439689cb65aa748befefb93f09a8954
BrownHat <caubedondoc.ls01@gmail.com>
2016-09-08 23:38:57 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -24,7 +24,7 @@
           window.bot.chatUtilities.spam.push(spamWords[i]);
         }
 
-        // Example code for a bot command:
+
         bot.commands.baconCommand = {
           command: 'discord',  // The command to be called. With the standard command literal this would be: !bacon
           rank: 'user', // Minimum user permission to use the command
@@ -43,10 +43,21 @@
           rank: 'user', // Minimum user permission to use the command
           type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
           functionality: function (chat, cmd) {
-            //if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
               API.sendChat("/me http://moesocial.com");
+            }
+          }
+        };
+
+        bot.commands.baconCommand = {
+          command: 'fb',  // The command to be called. With the standard command literal this would be: !bacon
+          rank: 'user', // Minimum user permission to use the command
+          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+          functionality: function (chat, cmd) {
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+              API.sendChat("/me http://fb.com");
             }
           }
         };
