5af2027336896ace3da961880011abf0e3794fd6
BrownHat <caubedondoc.ls01@gmail.com>
2016-09-09 10:45:10 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -37,8 +37,8 @@
           }
         };
         
-        bot.commands.maoCommand = {
-          command: 'mao',  
+        bot.commands.abcCommand = {
+          command: 'abc',  
           rank: 'user',
           type: 'exact',
           functionality: function (chat, cmd) {
@@ -124,6 +124,19 @@
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
               API.sendChat("yui nii-sama!!!");
+            }
+          }
+        };
+        
+        bot.commands.maoCommand = {
+          command: 'mao', 
+          rank: 'user', 
+          type: 'exact',
+          functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+              API.sendChat("Mao lõm - sama!!!");
             }
           }
         };
