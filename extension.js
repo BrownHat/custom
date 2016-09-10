f3b6933510034b57b5a8404a2450710821c0e29c
BrownHat <caubedondoc.ls01@gmail.com>
2016-09-11 00:00:12 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -154,6 +154,19 @@
           }
         };
         
+        bot.commands.randomqCommand = {
+          command: 'randomquote', 
+          rank: 'user', 
+          type: 'exact',
+          functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+              API.sendChat(bot.chat.cookies);
+            }
+          }
+        };
+        
         // Load the chat package again to account for any changes
         bot.loadChat();
 
@@ -174,7 +187,7 @@
       autowoot: true,
       autoskip: false,
       smartSkip: true,
-      cmdDeletion: true,
+      cmdDeletion: false,
       maximumAfk: 120,
       afkRemoval: false,
       maximumDc: 60,
